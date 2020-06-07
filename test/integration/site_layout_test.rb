f301c5fae3bd2da6a4c9dde4b978d10b0c24c82e
yayoi sugiyama <yayoi.3080@icloud.com>
2020-06-07 23:58:05 +0000

    login logout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
+    
     #enshu 5.4.2.3
     get signup_path
     assert_select "title",full_title("Sign up")
