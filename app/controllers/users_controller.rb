76c2a6930ccb38587f9dd794d0a5a3980dacd0a2
yayoi sugiyama <yayoi.3080@icloud.com>
2020-06-11 16:01:20 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,41 +4,41 @@ class UsersController < ApplicationController
   
   def index
     @users = User.paginate(page: params[:page])
-  end  
-  
+  end
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)  # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # 更新に成功した場合を扱う。
-      flash[:success] = "プロフィールが変更されました"
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
   end
+
   
   def destroy
     @user = User.find(params[:id])
@@ -54,8 +54,7 @@ private
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
-   # ログイン済みユーザーかどうか確認
+ # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
         store_location
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  host = 'https://babb90e3bc094adb8b5e3ca7a18e4204.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
