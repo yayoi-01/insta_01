91bd985ecabf3eb2620e5888d133629f5fe1793b
yayoi sugiyama <yayoi.3080@icloud.com>
2020-06-18 14:33:42 +0000

    フォローアンフォロー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,13 @@ users = User.order(:created_at).take(6)
   users.each{ |user| user.microposts.create!(content:content) }
 end  
 
+# 以下のリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 
 
