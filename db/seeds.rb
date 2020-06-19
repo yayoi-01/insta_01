9e357ecdff4ed5d7d80abf76d38ea563ad13393f
yayoi sugiyama <yayoi.3080@icloud.com>
2020-06-19 12:32:00 +0000

    seederror1

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,7 @@ users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each{ |user| user.microposts.create!(content:content) }
+    
 end  
 
 # 以下のリレーションシップを作成する
