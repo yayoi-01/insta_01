7b5a2d45e2148d99e37e7d61bc698feb1e80bb36
yayoi sugiyama <yayoi.3080@icloud.com>
2020-06-21 12:43:41 +0000

    いいね機能途中まで実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :likes, dependent: :destroy
+  has_many :iine_users, through: :likes, source: :user
   default_scope -> { order(created_at: :desc) }
   scope :search_by_keyword, -> (keyword) {
     where("microposts.content LIKE :keyword", keyword: "%#{sanitize_sql_like(keyword)}%") if keyword.present?
@@ -13,9 +15,22 @@ class Micropost < ApplicationRecord
                                       message: "ファイルサイズは5MBまでです" }
   # 表示用のりサイズ済み画像を返す
   def display_image
-    image.variant({resize:"300x300^",crop:"300x300+0+0",gravity: :center})
+    image.variant({resize:"250x250^",crop:"250x250+0+0",gravity: :center})
   end  
   
-  
+    # マイクロポストをいいねする
+  def iine(user)
+    likes.create(user_id: user.id)
+  end
+
+  # マイクロポストのいいねを解除する（ネーミングセンスに対するクレームは受け付けません）
+  def uniine(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+ 
+  # 現在のユーザーがいいねしてたらtrueを返す
+  def iine?(user)
+    iine_users.include?(user)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
-
-end
+  resources :likes,               only: [:create, :destroy]
+  end
